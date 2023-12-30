ea481d97d9395cc969d97120a02ee2f26fa5180e
veerabhadrasai <veeraallamsetti@gmail.com>
2023-12-30 21:40:01 +0530

    add navbar and contact pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import J from './j.js';
+import Contact from './contact.js';
+import Navbar from './navbar.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <Routes>
          <Route path='/' element={<J/>}/>
+         <Route path='/Contact' element={<Contact/>}/>
      </Routes>
     </div>
   );
